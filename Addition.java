a68f75f7eeb2cb9cb6a9e715aa4fc4e69f63e0fb
DeepakPuligndla <deepak.puligundla@yahoo.com>
2015-12-08 12:54:03 -0800

    Working on some simple code

--- Addition.java
+++ Addition.java
@@ -1,16 +1,6 @@
 
-import static junit.Assertion.assertEquals;
-import junit.Test;
-
 public class Addition
 {
-
-	@Test
-	public void additionTest()
-	{
-		assertEquals("Test Result: ", 30, addition(10, 20));
-	}
-
 	public int addition(int x, int y)
 	{
 		return x + y;
--- test.java
+++ test.java
@@ -1,20 +1,8 @@
-import junit.runner.JUnitCore;
-import junit.runner.Result;
-import junit.runner.notification.Failure;
 
 public class addition
 {
 	public static void main(String args[])
 	{
-		Result result = JUnitCore.runClasses(Addition.class);
-		for (Failure failure : result.getFailure())
-		{
-			System.out.println(result.toString());
-		}
-
-		if (result.wasSuccessful())
-		{
-			System.out.println("Test case passed!");
-		}
+		System.out.println("hello!");
 	}	
 }
